4d2bea541f6ab9646b8a01ba52ad67754553dce1
DzOlha <monsarda11@gmail.com>
2022-09-12 21:18:43 +0300

    Created Queue class with necessary functions.

--- lab_2/Queue.java
+++ lab_2/Queue.java
@@ -1,2 +1,38 @@
-package lab_2;public class Queue {
+package lab_2;
+
+public class Queue {
+    private int n;
+    private boolean foundBear = false;
+    private boolean valueSet = false;
+    public boolean getFoundBear(){
+        return foundBear;
+    }
+    public void setFoundBear(){
+        this.foundBear = true;
+    }
+    synchronized int getRowNumber() {
+        while (!valueSet)
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println("InterruptedException caught");
+            }
+        System.out.println("Got: " + n);
+        valueSet = false;
+        notify();
+        return n;
+    }
+    synchronized void putRowNumber(int n) {
+        while(valueSet)
+            try {
+                wait();
+            } catch(InterruptedException e) {
+                System.out.println("InterruptedException caught");
+            }
+
+        this.n = n;
+        valueSet = true;
+        System.out.println("Put: " + n);
+        notify();
+    }
 }
