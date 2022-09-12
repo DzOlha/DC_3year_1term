391e2452fc69d5725ed6615b33f74fa62f8b0da8
DzOlha <monsarda11@gmail.com>
2022-09-12 22:07:04 +0300

    Added basic operations with queue.

--- lab_2/Queue.java
+++ lab_2/Queue.java
@@ -1,9 +1,83 @@
 package lab_2;
+class QueueClassOperation
+{
+    protected int[] arr;
+    protected int front;
+    protected int rear;
+    protected int capacity;
+    protected int count;
 
-public class Queue {
-    private int n;
+    public QueueClassOperation(int size)
+    {
+        arr = new int[size];
+        capacity = size;
+        front = 0;
+        rear = -1;
+        count = 0;
+    }
+
+    public int dequeue()
+    {
+        if (isEmpty())
+        {
+            System.out.println("Underflow\nProgram Terminated");
+            System.exit(-1);
+        }
+
+        int x = arr[front];
+
+        System.out.println("Removing " + x);
+
+        front = (front + 1) % capacity;
+        count--;
+
+        return x;
+    }
+
+    public void enqueue(int item)
+    {
+        if (isFull())
+        {
+            System.out.println("Overflow\nProgram Terminated");
+            System.exit(-1);
+        }
+
+        System.out.println("Inserting " + item);
+
+        rear = (rear + 1) % capacity;
+        arr[rear] = item;
+        count++;
+    }
+
+    public int peek()
+    {
+        if (isEmpty())
+        {
+            System.out.println("Underflow\nProgram Terminated");
+            System.exit(-1);
+        }
+        return arr[front];
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public boolean isEmpty() {
+        return (size() == 0);
+    }
+
+    public boolean isFull() {
+        return (size() == capacity);
+    }
+}
+
+public class Queue extends QueueClassOperation{
     private boolean foundBear = false;
     private boolean valueSet = false;
+    public Queue(int size){
+        super(size);
+    }
     public boolean getFoundBear(){
         return foundBear;
     }
@@ -17,6 +91,7 @@ public class Queue {
             } catch (InterruptedException e) {
                 System.out.println("InterruptedException caught");
             }
+        int n = this.dequeue();
         System.out.println("Got: " + n);
         valueSet = false;
         notify();
@@ -30,7 +105,7 @@ public class Queue {
                 System.out.println("InterruptedException caught");
             }
 
-        this.n = n;
+        this.enqueue(n);
         valueSet = true;
         System.out.println("Put: " + n);
         notify();
