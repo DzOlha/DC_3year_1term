ef3c6b01a5bbba390854ee5420b5c9ec85be571f
DzOlha <monsarda11@gmail.com>
2022-09-12 21:20:01 +0300

    Created Consumer class.

--- lab_2/Consumer.java
+++ lab_2/Consumer.java
@@ -1,2 +1,24 @@
-package lab_2;public class Consumer {
+package lab_2;
+
+public class Consumer implements Runnable{
+    private Queue q;
+    private boolean[][] Forest;
+    public Consumer(Queue q, boolean[][] f){
+        this.q = q;
+        this.Forest = f;
+        new Thread(this, "Consumer").start();
+    }
+    @Override
+    public void run() {
+        while(!Thread.currentThread().isInterrupted()){
+            int row = q.getRowNumber();
+            for(int j = 0; j < 100; j++){
+                if(Forest[row][j] == true) {
+                    q.setFoundBear();
+                    System.out.println("Found Bear! Coordinates" + row + "; " + j);
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
 }
