8d31ea2032ae804d89119df1944cc7f90f312f6d
DzOlha <monsarda11@gmail.com>
2022-09-12 21:19:19 +0300

    Created Producer class.

--- lab_2/Producer.java
+++ lab_2/Producer.java
@@ -1,2 +1,16 @@
-package lab_2;public class Producer {
+package lab_2;
+
+public class Producer implements Runnable {
+    private Queue q;
+    public Producer(Queue q){
+        this.q = q;
+        new Thread(this, "Producer").start();
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 100; i++){
+            q.putRowNumber(i);
+        }
+    }
 }
