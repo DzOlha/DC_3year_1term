21228e8018f96537933127e7dbe3ab8df9a51154
DzOlha <monsarda11@gmail.com>
2022-11-19 17:11:44 +0200

    Modified the description files.

--- lab_3/part_2/Program.java
+++ lab_3/part_2/Program.java
@@ -1,2 +1,24 @@
-package lab_3.part_2;public class Program {
+package lab_3.part_2;
+
+public class Program {
+    public static void main(String[] args) {
+        int numberOfVisitors = 10;
+
+        Thread[] threads = new Thread[numberOfVisitors];
+        Barber barber = new Barber();
+
+        for (int i = 0; i < numberOfVisitors; i++) {
+            threads[i] = new Thread(new Visitor(barber), "Visitor №" + i);
+            threads[i].start();
+        }
+
+        for (int i =0; i<numberOfVisitors; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
--- lab_3/part_2/Visitor.java
+++ lab_3/part_2/Visitor.java
@@ -1,2 +1,15 @@
-package lab_3.part_2;public class Visitor {
+package lab_3.part_2;
+
+public class Visitor implements Runnable {
+    private final Barber barber;
+
+    public Visitor(Barber barber) {
+        this.barber = barber;
+    }
+
+    @Override
+    public void run() {
+        barber.addVisitorToQueue(this);
+    }
 }
+
