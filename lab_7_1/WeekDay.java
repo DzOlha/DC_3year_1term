8e5d41af526ca8fbc36f118bebb455fb786f8a39
DzOlha <monsarda11@gmail.com>
2022-11-19 17:19:31 +0200

    Make the 'code' field final to avoid its changes.

--- lab_7_1/WeekDay.java
+++ lab_7_1/WeekDay.java
@@ -3,7 +3,7 @@ package lab_7_1;
 import java.util.ArrayList;
 
 public class WeekDay {
-    public Integer code;
+    public final Integer code;
     public String name;
     ArrayList<Lesson> lessons;
     public WeekDay(Integer code, String name){
